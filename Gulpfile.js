40d9953a4b9b3e924f4d5210b0a9d66e49de0625
Bryan Morris <bryan@internalfx.com>
2015-07-24 15:52:07 -0700

    add gulp config

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,1 +1,21 @@
 
+var gulp = require('gulp')
+var webpack = require('webpack')
+var wpConf = require('./webpack.config.js')
+var gutil = require('gulp-util')
+var mocha = require('gulp-mocha')
+
+gulp.task('default', ['webpack'])
+
+gulp.task('test', ['webpack'], function () {
+  return gulp.src('test/index.js', {read: false})
+  .pipe(mocha({}))
+})
+
+gulp.task('webpack', function (callback) {
+  webpack(wpConf, function (err, stats) {
+    if (err) throw new gutil.PluginError('webpack', err)
+    gutil.log('[webpack]', stats.toString({}))
+    callback()
+  })
+})
